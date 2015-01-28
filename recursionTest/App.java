48d89027cfcd835e8df1fecca3d5dda16e870279
vivanovs <ivanovs223@gmail.com>
2015-01-28 11:47:58 +0000

    Added McCarthy91 method

--- recursionTest/App.java
+++ recursionTest/App.java
@@ -1,7 +1,6 @@
 package recursionTest;
 
 class Number {
-	
 
 	public int calculateFactorial(int n) {
 		if (n == 1) {
@@ -11,58 +10,66 @@ class Number {
 			return result;
 		}
 	}
-	
-	public int calculateFib(int n){
-		if ((n==1) || (n==2)){
+
+	public int calculateFib(int n) {
+		if ((n == 1) || (n == 2)) {
 			return 1;
-		}else{
-			int result = calculateFib(n-1) + calculateFib(n-2);
+		} else {
+			int result = calculateFib(n - 1) + calculateFib(n - 2);
 			return result;
 		}
 	}
-	
+
 	/** Stack overflow test */
-	
-	public void toInfinity(int n){
-		int n2 = n+1;
+
+	public void toInfinity(int n) {
+		int n2 = n + 1;
 		System.out.println(n2);
 		toInfinity(n2);
-		
+
 	}
-	
-	public void printOdd(int n){
-		if (n>0){
+
+	public void printOdd(int n) {
+		if (n > 0) {
 			System.out.println(n);
 			n = n - 2;
 			printOdd(n);
 		}
 	}
-	
-	public void printNumbers(int n){
-		if (n<=0){
+
+	public void printNumbers(int n) {
+		if (n <= 0) {
 			return;
 		}
 		System.out.println(n);
-		printNumbers(n-2);
-		//printNumbers(n-3);
+		printNumbers(n - 2);
+		// printNumbers(n-3);
 		System.out.println(n);
 	}
-	
-	
-	
+
+	public int mcCarthy91(int n) {
+		System.out.println(n);
+		if (n > 100) {
+			return n - 10;
+		} else {
+			return mcCarthy91(mcCarthy91(n + 11));
+		}
+	}
 
 }
 
 public class App {
 	public static void main(String[] args) {
 		Number fact1 = new Number();
+
+		// System.out.println(fact1.calculateFactorial(5));
+		// System.out.println(fact1.calculateFib(6));
+
+		// fact1.printNumbers(6);
+
+		// fact1.printOdd(123);
+		// fact1.toInfinity(1);
 		
-//		System.out.println(fact1.calculateFactorial(5));
-//		System.out.println(fact1.calculateFib(6));
-		
-		fact1.printNumbers(6);
-		
-//		fact1.printOdd(123);
-//		fact1.toInfinity(1);
+		fact1.mcCarthy91(73);
 	}
 }
