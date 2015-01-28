d446ec0f545e7f7f4b743a0d5e1f7b0c2c434748
vivanovs <ivanovs223@gmail.com>
2015-01-28 10:37:11 +0000

    Method factorial added

--- recursionTest/App.java
+++ recursionTest/App.java
@@ -1,9 +1,23 @@
 package recursionTest;
 
-public class App {
-public static void main(String[] args) {
-	
-	
-	
+class Factorial {
+	int num = 0;
+
+	public int getFactorial(int n) {
+		if (n == 1) {
+			return 1;
+		} else {
+			num = n * getFactorial(n - 1);
+			return num;
+		}
+	}
+
 }
+
+public class App {
+	public static void main(String[] args) {
+		Factorial fact1 = new Factorial();
+		
+		System.out.println(fact1.getFactorial(5));
+	}
 }
